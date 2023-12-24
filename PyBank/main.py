89c33e48e41e83f6db1a9b25dfe607dcfa5d106b
Travis Cook <justuscooks@gmail.com>
2023-12-24 15:39:42 -0800

    Main loop minus average

---
+++ PyBank/main.py
@@ -0,0 +1,63 @@
+import os
+import csv
+
+csvPath = os.path.join("Resources", "budget_data.csv")
+
+monthCount = 0
+totalProfitLoss = 0
+averageChange = 0
+greatestIncrease = ["", 0]
+greatestDecrease = ["", 0]
+
+prevTotal = 0
+currentTotal = 0
+monthlyDifference = []
+change = 0
+
+
+
+with open(csvPath, 'r') as budgetCSV:
+    csvReader = csv.reader(budgetCSV, delimiter=",")
+    csvHeader = next(csvReader)
+    print(csvHeader)
+
+    for row in csvReader:
+        currentTotal= int(row[1])
+        if prevTotal != 0:
+            monthlyDifference.append(currentTotal - prevTotal)
+            change=change-int(currentTotal)
+            
+            #updates greatest increase/decrease
+            if currentTotal - prevTotal > greatestIncrease[1]:
+                greatestIncrease = [row[0], currentTotal - prevTotal]
+            elif currentTotal - prevTotal < greatestDecrease[1]:
+                greatestDecrease = [row[0], (currentTotal - prevTotal)]
+
+            
+
+
+
+            prevTotal = int(row[1])
+
+        else:      #sets initial values on the first row of data      
+            prevTotal = int(row[1])
+            change = int(row[1])
+
+
+        monthCount +=1
+        totalProfitLoss += int(row[1])
+
+
+print(len(monthlyDifference))
+print(change)
+
+# generate final report
+print("Financial Analysis")
+print("----------------------------")
+print (f"Total Months:  {monthCount}")
+print(f"Total:  ${totalProfitLoss}")
+# print(f"Average Change:  ${averageChange}")
+print(f"Greatest Increase in Profits: {greatestIncrease[0]} (${greatestIncrease[1]})")
+print(f"Greatest Decrease in Profits: {greatestDecrease[0]} (${greatestDecrease[1]})")
+
+
