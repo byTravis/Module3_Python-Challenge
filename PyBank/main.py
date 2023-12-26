1d6d76fedbf1ce17a1ebca50b329748a82db7a08
Travis Cook <justuscooks@gmail.com>
2023-12-26 10:32:25 -0800

    working code with print statement

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+# establish varaibles and default values
 csvPath = os.path.join("Resources", "budget_data.csv")
 
 monthCount = 0
@@ -12,49 +13,42 @@ greatestDecrease = ["", 0]
 prevTotal = 0
 currentTotal = 0
 monthlyDifference = []
-change = 0
 
 
-
+# iterate through CSV and calculate values
 with open(csvPath, 'r') as budgetCSV:
     csvReader = csv.reader(budgetCSV, delimiter=",")
     csvHeader = next(csvReader)
-    print(csvHeader)
 
     for row in csvReader:
         currentTotal= int(row[1])
-        if prevTotal != 0:
-            monthlyDifference.append(currentTotal - prevTotal)
-            change=change+(currentTotal - prevTotal)
-            
+        if prevTotal != 0:  
+            monthlyDifference.append(currentTotal - prevTotal)           
             #updates greatest increase/decrease
-            if currentTotal - prevTotal > greatestIncrease[1]:
+            if currentTotal - prevTotal > greatestIncrease[1]:  #updates greatest increase
                 greatestIncrease = [row[0], currentTotal - prevTotal]
-            elif currentTotal - prevTotal < greatestDecrease[1]:
+            elif currentTotal - prevTotal < greatestDecrease[1]:  #updates greatest decrease
                 greatestDecrease = [row[0], (currentTotal - prevTotal)]
 
-            
-
-
-
-            prevTotal = int(row[1])
+            prevTotal = currentTotal  #sets previos total to currennt total for next loop
 
         else:      #sets initial values on the first row of data      
-            prevTotal = int(row[1])
-
-
+            prevTotal = currentTotal  #first record has no previous record and will always be current total
 
         monthCount +=1
-        totalProfitLoss += int(row[1])
+        totalProfitLoss += int(row[1])  #running total for total profit/loss
 
-
+# calculates average monthly change
+for change in monthlyDifference:
+    averageChange +=change
+averageChange= averageChange/len(monthlyDifference)
 
 # generate final report
 print("Financial Analysis")
 print("----------------------------")
 print (f"Total Months:  {monthCount}")
 print(f"Total:  ${totalProfitLoss}")
-print(f"Average Change:  $" + str(round(change/(monthCount-1), 2)))
+print(f"Average Change:  $" + str(round(averageChange, 2)))
 print(f"Greatest Increase in Profits: {greatestIncrease[0]} (${greatestIncrease[1]})")
 print(f"Greatest Decrease in Profits: {greatestDecrease[0]} (${greatestDecrease[1]})")
 
